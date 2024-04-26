05bef96479e86b3adbadf284074de349316e7b09
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-04-27 00:46:17 +0800

    Update RPC_manager.py to use Raft stock message definitions

--- src/server/RPC_manager.py
+++ src/server/RPC_manager.py
@@ -1,16 +1,16 @@
 from src.message import raft_stock_pb2_grpc, raft_stock_pb2
 
-class RPCManager(raft_pb2_grpc.RaftServiceServicer):
+class RPCManager(raft_stock_pb2_grpc.RaftServiceServicer):
     def __init__(self, state_machine):
         self.state_machine = state_machine
 
     def register(self, server):
-        raft_pb2_grpc.add_RaftServiceServicer_to_server(self, server)
+        raft_stock_pb2_grpc.add_RaftServiceServicer_to_server(self, server)
 
     def RequestVote(self, request, context):
         # Election voting logic here
-        return raft_pb2.VoteResponse(term=self.state_machine.term, voteGranted=True)
+        return raft_stock_pb2.VoteResponse(term=self.state_machine.term, voteGranted=True)
 
     def AppendEntries(self, request, context):
         # Append entries logic here
-        return raft_pb2.EntriesResponse(term=self.state_machine.term, success=True)
+        return raft_stock_pb2.EntriesResponse(term=self.state_machine.term, success=True)
