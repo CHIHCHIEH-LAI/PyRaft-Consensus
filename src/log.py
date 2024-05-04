9219fd932597b0cbb3832ece3607370e7b848d45
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-04 13:44:20 +0800

    Refactor log.py to use Transaction class for log entries

--- src/log.py
+++ src/log.py
@@ -1,20 +1,13 @@
-from typing import List
-import datetime
-from enum import Enum
-
-class TransactionType(Enum):
-    BUY = "buy"
-    SELL = "sell"
+from src.transaction import Transaction
+from collections import defaultdict
 
 class LogEntry:
-    def __init__(self, user_id: str, stock_id: str, quantity: int, price: float, timestamp: datetime.datetime, transaction_type: TransactionType):
-        self.user_id = user_id
-        self.stock_id = stock_id
-        self.quantity = quantity
-        self.price = price
-        self.timestamp = timestamp
-        self.transaction_type = transaction_type
+    def __init__(self, leaderId: int, logTerm: int, logIndex: int, transaction: Transaction):
+        self.leaderId = leaderId
+        self.logTerm = logTerm
+        self.logIndex = logIndex
+        self.transaction = transaction
 
 class Log:
     def __init__(self):
-        self.entries: List[LogEntry] = []
+        self.entries = defaultdict(list)
---
+++ src/transaction.py
@@ -0,0 +1,15 @@
+import datetime
+from enum import Enum
+
+class TransactionType(Enum):
+    BUY = "buy"
+    SELL = "sell"
+
+class Transaction:
+    def __init__(self, user_id: str, stock_id: str, quantity: int, price: float, timestamp: datetime.datetime, transaction_type: TransactionType):
+        self.user_id = user_id
+        self.stock_id = stock_id
+        self.quantity = quantity
+        self.price = price
+        self.timestamp = timestamp
+        self.transaction_type = transaction_type
