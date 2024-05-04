10cdb03d2060921130156c95b8a977caa2089b40
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-04 11:48:32 +0800

    Refactor and remove unused code files

--- src/schema/transaction.py
+++
@@ -1,8 +0,0 @@
-class Transaction:
-    def __init__(self, userId, stockId, quantity, price, timestamp, transactionType):
-        self.userId = userId
-        self.stockId = stockId
-        self.quantity = quantity
-        self.price = price
-        self.timestamp = timestamp
-        self.transactionType = transactionType
