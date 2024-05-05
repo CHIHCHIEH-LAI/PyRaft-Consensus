c0264031b3c9b3f444d56f64f21f18131e7294dc
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-05 18:46:01 +0800

    chore: Update transaction.py variable name to follow naming convention

--- src/transaction.py
+++ src/transaction.py
@@ -6,10 +6,10 @@ class TransactionType(Enum):
     SELL = 1
 
 class Transaction:
-    def __init__(self, userId: str, stockId: str, quantity: int, price: float, timestamp: datetime.datetime, transaction_type: TransactionType):
+    def __init__(self, userId: str, stockId: str, quantity: int, price: float, timestamp: datetime.datetime, transactionType: TransactionType):
         self.userId = userId
         self.stockId = stockId
         self.quantity = quantity
         self.price = price
         self.timestamp = timestamp
-        self.transaction_type = transaction_type
+        self.transactionType = transactionType
