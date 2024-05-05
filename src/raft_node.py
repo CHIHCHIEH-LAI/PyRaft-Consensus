aa9a0aadc5f2d9d9235d213937c65d569530b3a2
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-05 15:26:01 +0800

    add run_election, state transition

--- src/raft_node.py
+++ src/raft_node.py
@@ -1,25 +1,49 @@
+from src.heartbeat_manager import HeartbeatManager
 from src.log import Log, LogEntry
 from src.state_machine import StateMachine
 from src.election_module import ElectionModule
 
 class RaftNode:
-    def __init__(self, nodeId: int):
+    def __init__(self, nodeId: int, memberTable: dict):
         self.nodeId = nodeId
+        self.memberTable = memberTable
         self.log = Log()
         self.state_machine = StateMachine()
-        self.election_module = ElectionModule(self.state_machine)
+        self.election_module = ElectionModule(memberTable)
+        self.heartbeat_manager = HeartbeatManager(memberTable)
 
-    def start(self):
-        pass
+    async def run(self):
+        while not self.state_machine.is_stopped():
+            if self.state_machine.is_leader():
+                await self.multicast_heartbeats()
+            elif self.state_machine.is_follower():
+                await self.wait_for_heartbeat()
+            elif self.state_machine.is_candidate():
+                await self.run_election()
+            else:
+                raise Exception('Invalid state')
 
     def stop(self):
-        pass
+        self.state_machine.to_stopped()
 
-    def wait_for_termination(self):
-        pass
+    def wait_for_heartbeat(self):
+        if self.heartbeat_manager.has_timed_out():
+            self.state_machine.to_candidate()
 
-    def append_log_entry(self, log_entry: LogEntry):
-        self.log.append_entry(log_entry)
+    async def run_election(self):
+        voteRequest = {
+            'term': self.state_machine.get_current_term(),
+            'candidateId': self.nodeId,
+            'lastLogIndex': self.log.get_last_index(),
+            'lastLogTerm': self.log.get_last_term()
+        }
+        success = await self.election_module.run_election(self.nodeId, voteRequest)
+        if success:
+            self.state_machine.to_leader()
+        else:
+            self.state_machine.to_follower()        
     
-    def vote_for_candidate(self, candidate_id):
-        self.election_module.vote_for_candidate(candidate_id)
+    async def multicast_heartbeats(self):
+        heartbeat = {
+            
+        }
