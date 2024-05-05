d15c27fa3aa4fdb966ca3f15dcb731694311be90
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-05 15:24:32 +0800

    use enum values instead of string names

--- src/state_machine.py
+++ src/state_machine.py
@@ -1,29 +1,43 @@
 from enum import Enum
 
 class StateType(Enum):
-    FOLLOWER = "follower"
-    CANDIDATE = "candidate"
-    LEADER = "leader"
+    FOLLOWER = 1
+    CANDIDATE = 2
+    LEADER = 3
+    STOPPED = 4
 
 class StateMachine:
     def __init__(self):
         self.state = StateType.FOLLOWER
         self.currentTerm = 0
 
-    def transition_to_leader(self):
+    def is_stopped(self):
+        return self.state == StateType.STOPPED
+    
+    def is_leader(self):
+        return self.state == StateType.LEADER
+
+    def is_follower(self):
+        return self.state == StateType.FOLLOWER
+    
+    def is_candidate(self):
+        return self.state == StateType.CANDIDATE
+    
+    def to_stopped(self):
+        self.state = StateType.STOPPED
+
+    def to_leader(self):
         self.state = StateType.LEADER
 
-    def transition_to_follower(self):
+    def to_follower(self):
         self.state = StateType.FOLLOWER
 
-    def transition_to_candidate(self):
+    def to_candidate(self):
         self.state = StateType.CANDIDATE
-
-    def get_state(self):
-        return self.state
+        self.set_current_term(self.get_current_term() + 1)
 
     def get_current_term(self):
         return self.currentTerm
     
-    def increment_current_term(self):
-        self.currentTerm += 1
+    def set_current_term(self, term: int):
+        self.currentTerm = term
