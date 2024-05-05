c38647651649f861b53d24a966e74028a2b81a80
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-05 20:42:39 +0800

    chore: Refactor server.py to use memberTable for serving multiple nodes

--- server.py
+++ server.py
@@ -5,8 +5,10 @@ from src.proto import raft_pb2_grpc
 from src.grpc_server import gRPCServer
 from src.raft_node import RaftNode
 
-async def serve(host, port):
-    raft_node = RaftNode()
+async def serve(id: int,  memberTable: dict):
+    raft_node = RaftNode(id, memberTable)
+
+    host, port = memberTable[id]
 
     server = aio.server()
     raft_pb2_grpc.add_RaftServiceServicer_to_server(gRPCServer(), server)
@@ -17,4 +19,21 @@ async def serve(host, port):
     await server.wait_for_termination()
 
 if __name__ == '__main__':
-    asyncio.run(serve(host='localhost', port=50051))
+
+    memberTable = {
+        1: ('localhost', 50051),
+        2: ('localhost', 50052),
+        3: ('localhost', 50053),
+        4: ('localhost', 50054),
+        5: ('localhost', 50055)
+    }
+
+    servers = [
+        serve(1, memberTable),
+        serve(2, memberTable),
+        serve(3, memberTable),
+        serve(4, memberTable),
+        serve(5, memberTable)
+    ]
+
+    asyncio.run(asyncio.gather(*servers))
