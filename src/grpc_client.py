0d2690723d5fede762b70db532f67474c8d72781
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-05 15:28:35 +0800

    implement request_vote and send_heartbeat rpc

--- src/grpc_client.py
+++ src/grpc_client.py
@@ -1,20 +1,28 @@
 import grpc
 from src.proto import raft_pb2
 from src.proto import raft_pb2_grpc
-from src.raft_node import RaftNode
 
 class gRPCClient:
-    def __init__(self, raft_node: RaftNode):
-        self.raft_node = raft_node
 
-    def make_append_entry_rpc(self):
-        pass
+    def make_append_entry_rpc(self, host: str, port: int):
+        url = self.construct_url(host, port)
+        
 
-    async def make_request_vote_rpc(self, url: str):
+    async def make_request_vote_rpc(self, host: str, port: int, voteRequest: dict):
+        url = self.construct_url(host, port)
         with grpc.insecure_channel(url) as channel:
             stub = raft_pb2_grpc.RaftServiceStub(channel)
-            response = await stub.RequestVote(raft_pb2.VoteRequest())
+            response = await stub.RequestVote(raft_pb2.VoteRequest(**voteRequest))
             return response.voteGranted
+        
+    async def make_send_heartbeat_rpc(self, host: str, port: int, heartbeat: dict):
+        url = self.construct_url(host, port)
+        with grpc.insecure_channel(url) as channel:
+            stub = raft_pb2_grpc.RaftServiceStub(channel)
+            await stub.SendHeartbeat(raft_pb2.Heartbeat(**heartbeat))
 
-    def make_add_transaction_rpc(self, url: str):
-        pass
+    def make_add_transaction_rpc(self, host: str, port: int, transaction: dict):
+        url = self.construct_url(host, port)
+        
+    def construct_url(self, host: str, port: int):
+        return f'{host}:{port}'
