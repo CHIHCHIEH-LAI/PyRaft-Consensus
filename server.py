592f0797dcd7aa00c72ba61840953aa48e2ecc80
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-05 15:24:50 +0800

    use async/await syntax for starting and stopping the server

--- server.py
+++ server.py
@@ -7,19 +7,14 @@ from src.raft_node import RaftNode
 
 async def serve(host, port):
     raft_node = RaftNode()
-    await raft_node.start()
 
     server = aio.server()
     raft_pb2_grpc.add_RaftServiceServicer_to_server(gRPCServer(), server)
     server.add_insecure_port(f'{host}:{port}')
     await server.start()
 
-    try:
-        await raft_node.wait_for_termination()
-        await server.wait_for_termination()
-    except KeyboardInterrupt:
-        await raft_node.stop()
-        await server.stop(0)
+    await raft_node.run()
+    await server.wait_for_termination()
 
 if __name__ == '__main__':
     asyncio.run(serve(host='localhost', port=50051))
