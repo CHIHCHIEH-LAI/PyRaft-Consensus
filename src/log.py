cb152fe3166aaa977110d398d9b4578c2839c2ce
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-05 15:57:04 +0800

    include lastTerm and provide methods to get last index and last term

--- src/log.py
+++ src/log.py
@@ -11,6 +11,13 @@ class LogEntry:
 class Log:
     def __init__(self):
         self.entries = defaultdict(list)
+        self.lastTerm = 0
+
+    def get_last_index(self):
+        return self.entries[self.lastTerm][-1].logIndex
+    
+    def get_last_term(self):
+        return self.lastTerm
     
     def append_entry(self, log_entry: LogEntry):
         pass
