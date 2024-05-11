b2c0790a560b7e1755d97bf9d62fb51e904ec923
CHIHCHIEH-LAI <jjyyjay@gmail.com>
2024-05-11 10:36:58 +0800

    Refactor server.py to use RaftService for starting the service using asyncio

--- server.py
+++ server.py
@@ -1,30 +1,10 @@
 import asyncio
 import logging
-import grpc
-import time
 
-from src.raft_node import RaftNode
-from src.channel.proto import raft_pb2_grpc
-from src.channel.grpc_server import gRPCServer
+from src.raft_service import RaftService
 
 # Configure the logging module
 logging.basicConfig(level=logging.INFO)
-
-async def serve(id: int, memberTable: dict):
-    logging.info(f'Starting server {id}')
-    raft_node = RaftNode(id, memberTable)
-    server = grpc.aio.server()
-    raft_pb2_grpc.add_RaftServiceServicer_to_server(gRPCServer(raft_node), server)
-    host, port = memberTable[id]
-    server.add_insecure_port(f'{host}:{port}')
-
-    await server.start()
-    logging.info(f'Server {id} started')
-    time.sleep(5)
-    await raft_node.run()
-    logging.info(f'Raft node {id} running')
-    await server.wait_for_termination()
-    logging.info(f'Server {id} terminated')
 
 async def start_service():
     memberTable = {
@@ -35,16 +15,13 @@ async def start_service():
         5: ('localhost', 50055)
     }
 
-    servers = [
-        serve(1, memberTable),
-        serve(2, memberTable),
-        serve(3, memberTable),
-        serve(4, memberTable),
-        serve(5, memberTable)
-    ]
-
+    serves = []
+    for id in range(1, 6):
+        raft_service = RaftService(id, memberTable)
+        serves.append(raft_service.serve())
+    
     logging.info('Starting all servers')
-    await asyncio.gather(*servers)
+    await asyncio.gather(*serves)
     logging.info('All servers terminated')
 
 if __name__ == '__main__':
---
+++ src/raft_service.py
@@ -0,0 +1,29 @@
+import asyncio
+import logging
+import grpc
+
+from src.raft_node import RaftNode
+from src.channel.proto import raft_pb2_grpc
+from src.channel.grpc_server import gRPCServer
+
+class RaftService:
+    def __init__(self, id: int, memberTable: dict):
+        self.id = id
+        logging.info(f'Starting server {id}')
+        self.raft_node = RaftNode(id, memberTable)
+        self.server = grpc.aio.server()
+
+        raft_pb2_grpc.add_RaftServiceServicer_to_server(gRPCServer(self.raft_node), self.server)
+        host, port = memberTable[id]
+        self.server.add_insecure_port(f'{host}:{port}')
+
+    async def serve(self):
+        await self.server.start()
+        logging.info(f'Server {self.id} started')
+        await asyncio.sleep(5)
+        await self.raft_node.run()
+        logging.info(f'Raft node {self.id} running')
+        await self.server.wait_for_termination()
+        logging.info(f'Server {self.id} terminated')
+
+
